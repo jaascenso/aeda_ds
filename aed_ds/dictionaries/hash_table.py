ef4c1a2b162b3e228e2e2ee42e03f6a72baa79b5
jaascenso <joao_a_ascenso@hotmail.com>
2020-05-21 01:10:15 +0100

    firts changes

--- aed_ds/dictionaries/hash_table.py
+++ aed_ds/dictionaries/hash_table.py
@@ -3,16 +3,25 @@ from ..exceptions import NoSuchElementException, DuplicatedKeyException
 from ..lists.singly_linked_list import SinglyLinkedList
 
 class HashTable(Dictionary):
-    def __init__(self, size=101):
-        self.key = key
-        self.value = value
+    def __init__(self, size = 101):
+        self.key = None
+        self.value = None
         SinglyLinkedList.__init__(self)
 
-    def size(self): pass
-        
-    def is_full(self): pass
+    def size(self):
+        return self.size
 
-    def get(self, k): pass
+    def is_full(self):
+        if self.key == None:
+            raise NoSuchElementException
+
+
+    def get(self, k , size): 
+        if k < 0 or k > size:
+            raise NoSuchElementException
+        
+
+
 
     def insert(self, k, v): pass
 
